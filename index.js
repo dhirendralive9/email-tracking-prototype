769b299a4724d47332bb61d8728734c57ac812ce
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-05-28 02:24:05 +0530

    added platform, fs  and others

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const fs = require('fs');
+const platform = require('platform');
+var report = [];
+var user;
 
+fs.readFile('./public/track.json',(err, data)=>{
+    err => console.log(err);
+    report = JSON.parse(data);
+    
+})
+
+app.use(express.static('public'))
 app.set('view engine','ejs');
 app.set('trust proxy', true)
 
 app.get('/',(req,res)=>{
     res.send("This is working")
-    console.log(req.headers['user-agent']);
+    user = platform.parse(req.headers['user-agent']);
+    console.log(user)
     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
     console.log(req.ip);
 });
