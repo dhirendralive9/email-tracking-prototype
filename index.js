2870d5d6cf857a57e6ddebd682a2bf58f6e73ec0
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-05-28 02:56:42 +0530

    tracker is almost ready

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const platform = require('platform');
+const date = require('date-and-time');
 var report = [];
 var user;
+var port = 5000;
 
 fs.readFile('./public/track.json',(err, data)=>{
     err => console.log(err);
@@ -18,15 +20,23 @@ app.set('trust proxy', true)
 app.get('/',(req,res)=>{
     res.send("This is working")
     user = platform.parse(req.headers['user-agent']);
-    console.log(user)
+    
     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-    console.log(req.ip);
+    
+    var newData = {"ip":ip,"description":user.description,"browser":user.name,"os":user.os.family,"version":user.os.version,"date":`${date.format(new Date(), 'YYYY/MM/DD')}`,"time":`${date.format(new Date(), 'HH:mm:ss')}`};
+    report.push(newData);
+    fs.writeFile(`./public/track.json`,JSON.stringify(report),(err)=> console.log(err));
+   
 });
 
 app.get('/demo',(req,res)=>{
     res.render('index.ejs');
 })
 
-app.listen(4500,()=>{
- console.log("app is working on port 4500");
+app.get('/fetch',(req,res)=>{
+    res.json(...report);
+})
+
+app.listen(port,()=>{
+ console.log("app is working on port:",port);
 })
